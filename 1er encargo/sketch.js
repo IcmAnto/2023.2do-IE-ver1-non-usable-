a931a67e1d124e6929d62cdf745d3c1c8af710af
Antonia.icm <antonia.campillay.m@mail.pucv.cl>
2023-08-25 00:24:34 -0400

    1er encargo, 1ver web, desde p5js

---
+++ 1er encargo/sketch.js
@@ -0,0 +1,31 @@
+let yPosition = 0; // Posición inicial en Y de las líneas
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255, 1); // Fondo blanco
+  
+  // Dibuja las líneas blancas intermitentes
+  for (let x = 0; x < width; x += 40) {
+    stroke(255);
+    line(x, yPosition, x + 20, yPosition + 20);
+  }
+  
+  yPosition += 2; // Incrementa la posición Y para que las líneas bajen en diagonal
+  
+  // Reinicia la posición Y cuando las líneas salen de la pantalla
+  if (yPosition > height) {
+    yPosition = -20;
+  }
+
+  fill(255);
+  circle(width/2, height/2, 350);
+
+  textFont('Italic');
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  fill(0);
+  text("Página en construcción...", width/2, height/2);
+}
